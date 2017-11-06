7cdcaf1cb12d16187674b2a1cab67c21e68f47f9
Len <lwkerr@ncsu.edu>
2017-11-06 17:38:06 -0500

    Started using CSIR acronym.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -8,6 +8,8 @@ import Register from './Register'
 import logo from '../caesar.png';
 import { slide as Menu } from 'react-burger-menu';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Header from './Header';
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
@@ -24,44 +26,35 @@ class App extends Component {
 
 
 
-    render () {
-
+    render() {
         return (
+            <div className="whole">
+                <Header />
+                <Menu noOverlay>
+                    <a id="home" className="menu-item" href="/">Home</a>
+                    <Dropdown name="Login" content={<Login />} />
+                    <Dropdown name="Register" content={<Register />} />
+                    <a id="contact" className="menu-item" href="/contact">My Issues</a>
+                    <a id="contact" className="menu-item" href="/contact">Email Officials</a>
+                    <a id="contact" className="menu-item" href="/contact">About</a>
+                    <a onClick={this.showSettings} className="menu-item--small" href="">Settings</a>
+                </Menu>
+                <LandingPage />
 
-              <div className='whole'>
-                  <Menu noOverlay>
-                      <a id="home" className="menu-item" href="/">Home</a>
-                      <Dropdown name="Login" content={<Login />} />
-                      <Dropdown name="Register" content={<Register />} />
-                      <a id="contact" className="menu-item" href="/contact">My Issues</a>
-                      <a id="contact" className="menu-item" href="/contact">Email Officials</a>
-                      <a id="contact" className="menu-item" href="/contact">About</a>
-                      <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-                  </Menu>
-                  <div className="App-header" align='center'>
-                    <img src={logo} className="App-logo" alt="logo" />
+                <div className='whole'>
 
-                    <h2>Welcome to Caesar</h2>
+
+
+
+                    <div className="App">
+                    </div>
                 </div>
-
-
-                  <div className="App">
-                      <LandingPage/>
-                  </div>
-              </div>
-
-
-
+            </div>
         );
     }
 
 
 
 }
-const style = {
-    margin: 15,
 
-
-
-};
 export default App;
